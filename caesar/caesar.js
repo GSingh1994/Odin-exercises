c7585294c112c73d16b0b36b891f7aca66787bc0
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-02-02 12:47:45 -0600

    Partially done this exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,55 @@
-const caesar = function() {
+const caesar = function (str, num) {
+    let strToChar = [];
+    for (i = 0; i < str.length; i++) {
+        if (str[i] !== ',' && str[i] !== '!' && str[i] !== ' ') {
+            strToChar.push(str.charCodeAt(i) + num)
+        } else {
+            strToChar.push(str[i])
+        }
+    }
 
+    let result = [];
+    for (i = 0; i < strToChar.length; i++) {
+        if (strToChar[i] !== ',' && strToChar[i] !== '!' && strToChar[i] !== ' ') {
+            result.push(String.fromCharCode(strToChar[i]))
+        } else {
+            result.push(strToChar[i])
+        }
+    }
+
+    // let result = String.fromCharCode(...strToChar);
+    // return result
+    // console.log(strToChar)
+    return result.join('')
 }
 
 module.exports = caesar
+
+
+
+// const caesar = function (str, num) {
+//     let strToChar = [];
+//     for (i = 0; i < str.length; i++) {
+//         if (str[i] !== ',' && str[i] !== '!' && str[i] !== ' ') {
+//             strToChar.push(str.charCodeAt(i) + num)
+//         } else {
+//             strToChar.push(str[i])
+//         }
+//     }
+
+//     let result = [];
+//     for (i = 0; i < strToChar.length; i++) {
+//         if (strToChar[i] !== ',' && strToChar[i] !== '!' && strToChar[i] !== ' ') {
+//             result.push(String.fromCharCode(strToChar[i]))
+//         } else {
+//             result.push(strToChar[i])
+//         }
+//     }
+
+    // let result = String.fromCharCode(...strToChar);
+    // return result
+//     console.log(strToChar)
+//     console.log(result.join(''))
+// }
+
+// caesar('Hello, World!', 5);
