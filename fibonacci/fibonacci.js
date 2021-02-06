66d70ab692c82e68948fd4e0bb3e585b5ada29c3
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-02-06 13:56:21 -0600

    Done

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+
+let fib = [];
+
+const fibonacci = function (num) {
+    if (Math.sign(+num) === -1) {
+        return "OOPS"
+    } else {
+        fib[0] = 1;
+        fib[1] = 1;
+        for (i = 2; i < +num; i++) {
+            fib[i] = fib[i - 2] + fib[i - 1]
+        }
+        return fib[+num - 1]
+    }
 
 }
-
 module.exports = fibonacci
+
+
+
+
+
+// [1,1,2,3,5,8,13...]
+// fibonacci("4")
+
+// console.log(+"-2")
