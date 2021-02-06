eb9df8dd01d8fe7a3da8de18979c2a102f10995d
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-02-06 13:56:10 -0600

    Done

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,29 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+    let result = arr.map(a => a.title)
 
+    return result
 }
 
+
 module.exports = getTheTitles;
+
+
+
+
+
+// const books = [
+//     {
+//         title: 'Book',
+//         author: 'Name'
+//     },
+//     {
+//         title: 'Book2',
+//         author: 'Name2'
+//     }
+// ]
+
+
+
+
+
+// getTheTitles(books)
